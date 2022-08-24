e6a5295cc5ea28e007ae7b6e966865bbc555533a
edmilsonmedeiross <edmilsonmedeiros1990@gmail.com>
2022-08-24 15:18:11 -0300

    adicionado exercicios

---
+++ Fundamentos/bloco04 introdução à JavaScript e Lógica de Programação/dia03/ex002/script.js
@@ -0,0 +1,11 @@
+/*Agora, desenvolva um algoritmo que é capaz de inverter uma palavra. Por exemplo, a palavra "banana" seria invertida para "ananab". Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente.*/
+
+
+let word = 'edmilson';
+let reverseWord = '';
+
+for (let index = word.length -1; index >=0; index -= 1) {
+  reverseWord += word [index]
+}
+
+console.log(reverseWord);
